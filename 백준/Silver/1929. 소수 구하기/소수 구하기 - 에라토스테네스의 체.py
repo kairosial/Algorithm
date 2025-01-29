0e4e9484daea1fad7bdc68bf7d4bd5b82fae074e
kairosial <syk.kairos@gmail.com>
2025-01-30 01:02:28 +0900

    소수 구하기 - 에라토스테네스의 체

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기 - 에라토스테네스의 체.py
@@ -0,0 +1,15 @@
+def sieve_of_eratosthenes(m, n):
+    sieve = [True] * (n+1)
+    sieve[0] = sieve[1] = False
+    for i in range(2, int(n**0.5)+1):
+        if sieve[i]:
+            sieve[i*i : n+1 : i] = [False] * len(sieve[i*i : n+1 : i])
+            # for j in range(i*i, n+1, i):
+            #     sieve[j] = False
+    prime_num_list = [i for i in range(m, n+1) if sieve[i]]
+    return prime_num_list
+
+m, n = map(int, input().split())
+result = sieve_of_eratosthenes(m, n)
+for i in result:
+    print(i)
