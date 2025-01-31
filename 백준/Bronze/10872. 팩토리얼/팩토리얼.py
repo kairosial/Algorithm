8bfc6ae17f44bec9930e9b866d68062ba0d44f5f
Seungyeon Kim <syk.kairos@gmail.com>
2025-01-31 21:03:48 +0900

    [Bronze III] Title: 팩토리얼, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,5 +1,7 @@
+def factorial_recursive(n):
+    if n <= 1:
+        return 1
+    return n * factorial_recursive(n-1)
+
 n = int(input())
-result = 1
-for i in range(n):
-    result *= (i + 1)
-print(result)
+print(factorial_recursive(n))
