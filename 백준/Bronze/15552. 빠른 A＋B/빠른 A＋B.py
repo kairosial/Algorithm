900c0d802de7994b2b40de74a2d08de6cf5be031
Seungyeon Kim <syk.kairos@gmail.com>
2025-02-01 20:08:17 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1116 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -2,4 +2,4 @@ import sys
 T = int(sys.stdin.readline().rstrip())
 for _ in range(T):
     A, B = map(int, sys.stdin.readline().rstrip().split())
-    print(A + B)
+    sys.stdout.write(str(A + B) + '\n')
