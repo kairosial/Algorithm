028c91198ae6e9f17495dc5cc3eafd6d48b04977
Seungyeon Kim <syk.kairos@gmail.com>
2025-02-20 23:15:59 +0900

    [Bronze V] Title: 문자와 문자열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.py
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.py
@@ -1,6 +1,3 @@
-input_str = input()
-input_int = int(input())
-
-for idx, str in enumerate(input_str):
-    if idx == input_int-1:
-        print(str)
+S = list(input())
+i = int(input())
+print(S[i-1])
