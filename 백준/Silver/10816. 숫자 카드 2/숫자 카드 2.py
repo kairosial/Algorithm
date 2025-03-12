12f90064e6a1c09d953feb7eb6ce3c75a240dbbc
Seungyeon Kim <syk.kairos@gmail.com>
2025-03-12 23:01:51 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 768 ms, Memory: 123940 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readlines()
+N = int(input[0].strip())
+card_list = list(map(int, input[1].strip().split()))
+M = int(input[2].strip())
+target_list = list(map(int, input[3].strip().split()))
+
+count_dict = {}
+for num in card_list:
+    if num in count_dict:
+        count_dict[num] += 1
+    else:
+        count_dict[num] = 1
+
+result = []
+for i in target_list:
+    try:
+        result.append(count_dict[i])
+    except:
+        result.append(0)
+
+print(*result)
