a02a2ca345104af772f1b6a3ecdfd88904f82b2a
Seungyeon Kim <syk.kairos@gmail.com>
2025-03-17 22:44:01 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,9 @@
+scale_list = list(map(int, input().split()))
+ascending = [1, 2, 3, 4, 5, 6, 7, 8]
+descending = [8, 7, 6, 5, 4, 3, 2, 1]
+if scale_list == ascending:
+    print('ascending')
+elif scale_list == descending:
+    print('descending')
+else:
+    print('mixed')
