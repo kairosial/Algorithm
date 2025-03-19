740a2ff38d749e375942e7e827c0905dd51d5fd8
Seungyeon Kim <syk.kairos@gmail.com>
2025-03-20 08:50:36 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,10 +1,11 @@
 import sys
 T = int(input())
-
-fib_01_list = [(1,0)]
-for i in range(1, 41):
-    fib_01_list.append((fib_01_list[i-1][1], fib_01_list[i-1][0]+fib_01_list[i-1][1]))
-
 for _ in range(T):
     N = int(sys.stdin.readline().strip())
-    sys.stdout.write(str(fib_01_list[N][0]) + ' ' + str(fib_01_list[N][1]) + '\n')
+    f0, f1 = 1, 0
+    if N == 0:
+        print(f0, f1)
+    else:
+        for i in range(1, N+1):
+            f0, f1 = f1, f0+f1
+        print(f0, f1)
