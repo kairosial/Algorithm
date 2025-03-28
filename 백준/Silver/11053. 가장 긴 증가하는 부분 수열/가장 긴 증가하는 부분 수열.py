7b99ec22a3856a4cc878cc9d0668c8b4597acb79
kairosial <syk.kairos@gmail.com>
2025-03-28 13:00:59 +0900

    DP - 최장 증가 부분 수열(LIS)_O(N^2)

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,6 +1,11 @@
 n = int(input())
 arr = list(map(int, input().split()))
+
+# dp 배열을 1로 초기화 (각 수열 원소 자체만으로도 길이가 1인 증가부분수열)
 dp = [1] * n
+
+# dp 배열에 각 원소별로 증가부분수열의 길이를 업데이트하는 전략
+# 각 원소에 대해 앞쪽 원소들을 확인하여 dp 값을 갱신
 for i in range(n):
     for j in range(i):
         if arr[j] < arr[i]:
