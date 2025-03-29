9b53c6996ac3b69e13d666ffce72f6729dd89257
Seungyeon Kim <syk.kairos@gmail.com>
2025-03-29 23:08:57 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 132 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# dp 배열을 1로 초기화 (각 수열 원소 자체만으로도 길이가 1인 감소부분수열)
+dp = [1] * n
+
+# dp 배열에 각 원소별로 감소부분수열의 길이를 업데이트하는 전략
+# 각 원소에 대해 앞쪽 원소들을 확인하여 dp 값을 갱신
+for i in range(n):
+    for j in range(i):
+        if arr[j] > arr[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+print(max(dp))
