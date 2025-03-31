d671c31e466dfc00a9f3fe7eade4fdeee37d4081
kairosial <syk.kairos@gmail.com>
2025-03-31 23:43:18 +0900

    [Silver III] Title: 파도반 수열, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,13 +1,15 @@
 import sys
 T = int(input())
 
-def padovan(seq, n):
-    return seq[n-1] + seq[n-5]
+# P(n) = P(n-1) + P(n-5)라는 점화식으로 풀었지만
+# P(n) = P(n-2) + P(n-3)라는 점화식도 가능함
+def padovan(sequence, n):
+    return sequence[n-1] + sequence[n-5]
 
-p_seq = [0, 1, 1, 1, 2]
+dp = [0, 1, 1, 1, 2]
 for i in range(5, 101):
-    p_seq.append(padovan(p_seq, i))
+    dp.append(padovan(dp, i))
 
 for _ in range(T):
     N = int(sys.stdin.readline().strip())
-    sys.stdout.write(str(p_seq[N]) + '\n')
+    sys.stdout.write(str(dp[N]) + '\n')
