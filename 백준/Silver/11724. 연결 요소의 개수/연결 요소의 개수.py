f46d1c0747d041819e07e505fcbee173d2599568
Seungyeon Kim <syk.kairos@gmail.com>
2025-04-02 01:46:33 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 644 ms, Memory: 67200 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -3,27 +3,36 @@ from collections import deque
 
 n, m = map(int, input().split())
 graph = [[] for _ in range(n+1)]
-for i in range(1, m+1):
+for _ in range(1, m+1):
     a, b = map(int, sys.stdin.readline().strip().split())
     graph[a].append(b)
     graph[b].append(a)
 
-# DFS 알고리즘으로 실행 시 RecursionError 발생하므로 BFS 알고리즘 활용
-def bfs(graph, start, visited):
-    queue = deque([start])
-    visited[start] = True
+# DFS 알고리즘으로 실행 시 RecursionError 발생하므로 재귀 제한을 늘려주는 코드 추가 필요
+sys.setrecursionlimit(10 ** 5)
+def dfs(graph, node, visited):
+    # 현재 노드를 방문 처리
+    visited[node] = True
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for child in graph[node]:
+        if not visited[child]:
+            dfs(graph, child, visited)
+
+def bfs(graph, start_node, visited):
+    queue = deque([start_node])
+    visited[start_node] = True
     while queue:
-        v = queue.popleft()
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
+        node = queue.popleft()
+        for child in graph[node]:
+            if not visited[child]:
+                queue.append(child)
+                visited[child] = True
 
 result = 0
 visited = [False] * (n+1)
-for node in range(1, n+1):
-    if not visited[node]:
-        bfs(graph, node, visited)
+for i in range(1, n+1):
+    if not visited[i]:
+        dfs(graph, i, visited)
         result += 1
 
 print(result)
