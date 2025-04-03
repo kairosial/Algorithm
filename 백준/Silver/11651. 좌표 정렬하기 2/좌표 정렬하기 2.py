d39d2093e8302ce003a66888363175fee7b52cad
kairosial <syk.kairos@gmail.com>
2025-04-03 23:46:05 +0900

    lambda

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -1,11 +1,13 @@
 import sys
 
 N = int(input())
+# 모든 좌표 추가
 points = []
 for _ in range(N):
     x, y = map(int, sys.stdin.readline().strip().split())
     points.append((x, y))
 
+# y좌표 기준으로 오름차순 정렬, y좌표가 같은 경우에는 x좌표 기준으로 오름차순 정렬
 sorted_points = sorted(points, key=lambda point: (point[1], point[0]))
 
 for x, y in sorted_points:
