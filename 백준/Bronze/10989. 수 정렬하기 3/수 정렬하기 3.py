ddbb9d3c1d5df3377ee783909ea12ca8de8e9441
Seungyeon Kim <syk.kairos@gmail.com>
2025-07-08 23:30:54 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 6568 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+count = [0] * 10001
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    count[num] += 1
+
+for i in range(10001):
+    if count[i]:
+        for _ in range(count[i]):
+            sys.stdout.write((f'{i}\n'))
