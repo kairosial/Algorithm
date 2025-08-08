07e835b5ecd8fec2fa44b39843dfef071fedc8fc
Seungyeon Kim <syk.kairos@gmail.com>
2025-08-08 22:15:35 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+submul = []
+for i in range(1, n+1):
+    if n % i == 0:
+        submul.append(i)
+    if len(submul) == k:
+        break
+try:
+    print(submul[k-1])
+except:
+    print(0)
